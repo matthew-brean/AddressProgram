98a1b031aa10d8f4c29610039c9ca5c955aaf42b
matthew-brean <matthew.brean@ocsbstudent.ca>
2015-11-02 13:20:18 -0500

    version 1

---
+++ AddressProgram/AddressProgram.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace AddressProgram
+{
+    partial class frmAddressProgram
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtAptNumber = new System.Windows.Forms.TextBox();
+            this.txtProvince = new System.Windows.Forms.TextBox();
+            this.txtCity = new System.Windows.Forms.TextBox();
+            this.txtStreetAdress = new System.Windows.Forms.TextBox();
+            this.txtPostalCode = new System.Windows.Forms.TextBox();
+            this.lblApt = new System.Windows.Forms.Label();
+            this.lblStreetAdress = new System.Windows.Forms.Label();
+            this.lblCity = new System.Windows.Forms.Label();
+            this.lblProvince = new System.Windows.Forms.Label();
+            this.lblPostalCode = new System.Windows.Forms.Label();
+            this.btnShow = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtAptNumber
+            // 
+            this.txtAptNumber.Location = new System.Drawing.Point(160, 34);
+            this.txtAptNumber.Name = "txtAptNumber";
+            this.txtAptNumber.Size = new System.Drawing.Size(100, 20);
+            this.txtAptNumber.TabIndex = 0;
+            // 
+            // txtProvince
+            // 
+            this.txtProvince.Location = new System.Drawing.Point(160, 112);
+            this.txtProvince.Name = "txtProvince";
+            this.txtProvince.Size = new System.Drawing.Size(100, 20);
+            this.txtProvince.TabIndex = 1;
+            // 
+            // txtCity
+            // 
+            this.txtCity.Location = new System.Drawing.Point(160, 86);
+            this.txtCity.Name = "txtCity";
+            this.txtCity.Size = new System.Drawing.Size(100, 20);
+            this.txtCity.TabIndex = 2;
+            // 
+            // txtStreetAdress
+            // 
+            this.txtStreetAdress.Location = new System.Drawing.Point(160, 60);
+            this.txtStreetAdress.Name = "txtStreetAdress";
+            this.txtStreetAdress.Size = new System.Drawing.Size(100, 20);
+            this.txtStreetAdress.TabIndex = 3;
+            // 
+            // txtPostalCode
+            // 
+            this.txtPostalCode.Location = new System.Drawing.Point(160, 138);
+            this.txtPostalCode.Name = "txtPostalCode";
+            this.txtPostalCode.Size = new System.Drawing.Size(100, 20);
+            this.txtPostalCode.TabIndex = 4;
+            // 
+            // lblApt
+            // 
+            this.lblApt.AutoSize = true;
+            this.lblApt.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApt.Location = new System.Drawing.Point(12, 37);
+            this.lblApt.Name = "lblApt";
+            this.lblApt.Size = new System.Drawing.Size(102, 20);
+            this.lblApt.TabIndex = 5;
+            this.lblApt.Text = "Apt. Number:";
+            // 
+            // lblStreetAdress
+            // 
+            this.lblStreetAdress.AutoSize = true;
+            this.lblStreetAdress.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStreetAdress.Location = new System.Drawing.Point(12, 60);
+            this.lblStreetAdress.Name = "lblStreetAdress";
+            this.lblStreetAdress.Size = new System.Drawing.Size(111, 20);
+            this.lblStreetAdress.TabIndex = 6;
+            this.lblStreetAdress.Text = "Street Adress:";
+            // 
+            // lblCity
+            // 
+            this.lblCity.AutoSize = true;
+            this.lblCity.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCity.Location = new System.Drawing.Point(12, 86);
+            this.lblCity.Name = "lblCity";
+            this.lblCity.Size = new System.Drawing.Size(39, 20);
+            this.lblCity.TabIndex = 7;
+            this.lblCity.Text = "City:";
+            // 
+            // lblProvince
+            // 
+            this.lblProvince.AutoSize = true;
+            this.lblProvince.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProvince.Location = new System.Drawing.Point(12, 112);
+            this.lblProvince.Name = "lblProvince";
+            this.lblProvince.Size = new System.Drawing.Size(73, 20);
+            this.lblProvince.TabIndex = 8;
+            this.lblProvince.Text = "Province:";
+            // 
+            // lblPostalCode
+            // 
+            this.lblPostalCode.AutoSize = true;
+            this.lblPostalCode.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPostalCode.Location = new System.Drawing.Point(12, 138);
+            this.lblPostalCode.Name = "lblPostalCode";
+            this.lblPostalCode.Size = new System.Drawing.Size(99, 20);
+            this.lblPostalCode.TabIndex = 9;
+            this.lblPostalCode.Text = "Postal Code:";
+            // 
+            // btnShow
+            // 
+            this.btnShow.Location = new System.Drawing.Point(160, 181);
+            this.btnShow.Name = "btnShow";
+            this.btnShow.Size = new System.Drawing.Size(75, 23);
+            this.btnShow.TabIndex = 10;
+            this.btnShow.Text = "Show";
+            this.btnShow.UseVisualStyleBackColor = true;
+            this.btnShow.Click += new System.EventHandler(this.btnShow_Click);
+            // 
+            // frmAddressProgram
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.btnShow);
+            this.Controls.Add(this.lblPostalCode);
+            this.Controls.Add(this.lblProvince);
+            this.Controls.Add(this.lblCity);
+            this.Controls.Add(this.lblStreetAdress);
+            this.Controls.Add(this.lblApt);
+            this.Controls.Add(this.txtPostalCode);
+            this.Controls.Add(this.txtStreetAdress);
+            this.Controls.Add(this.txtCity);
+            this.Controls.Add(this.txtProvince);
+            this.Controls.Add(this.txtAptNumber);
+            this.Name = "frmAddressProgram";
+            this.Text = "AddressProgram";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtAptNumber;
+        private System.Windows.Forms.TextBox txtProvince;
+        private System.Windows.Forms.TextBox txtCity;
+        private System.Windows.Forms.TextBox txtStreetAdress;
+        private System.Windows.Forms.TextBox txtPostalCode;
+        private System.Windows.Forms.Label lblApt;
+        private System.Windows.Forms.Label lblStreetAdress;
+        private System.Windows.Forms.Label lblCity;
+        private System.Windows.Forms.Label lblProvince;
+        private System.Windows.Forms.Label lblPostalCode;
+        private System.Windows.Forms.Button btnShow;
+    }
+}
+
---
+++ AddressProgram/AddressProgram.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AddressProgram
+{ 
+
+    public partial class frmAddressProgram : Form
+    {
+
+        public void AddressListing(string aptNumber, string streetAddress, string city, string province, string postalCode)
+        {
+            MessageBox.Show(aptNumber + "\n" + streetAddress + "\n" + city + "\n" + province + "\n" + postalCode, "Address Listing");
+        }
+
+        public void AddressListing(string streetAddress, string city, string province, string postalCode)
+        {
+            MessageBox.Show(streetAddress + "\n" + city + "\n" + province + "\n" + postalCode, "Address Listing");
+        }
+
+        public frmAddressProgram()
+        {
+            InitializeComponent();
+        }
+
+        private void btnShow_Click(object sender, EventArgs e)
+        {
+            string aptNumber;
+            string streetAddress;
+            string city;
+            string province;
+            string postalCode;
+
+            aptNumber = this.txtAptNumber.Text;
+            streetAddress = this.txtStreetAdress.Text; //Convert.ToString(this.txtStreetAdress);
+            city = this.txtCity.Text;
+            province = this.txtProvince.Text;
+            postalCode = this.txtPostalCode.Text;
+
+            AddressListing(aptNumber, streetAddress, city, province, postalCode);
+
+        }
+    }
+}
